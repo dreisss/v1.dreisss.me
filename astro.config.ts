45a15b6fd1827ed37fb8e6bd711c637825d2417b
Davi Reis <dreisss026@gmail.com>
2023-09-22 23:41:19 -0300

    :sparkles: feat: added configurations

---
+++ astro.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+
+import unocss from 'unocss/astro'
+
+export default defineConfig({
+  integrations: [
+    unocss({
+      injectReset: true
+    })
+  ]
+})
