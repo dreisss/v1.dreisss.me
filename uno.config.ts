09c5e55486ed8ed73efc68eebd5c536ada6e7990
Davi Reis <dreisss026@gmail.com>
2023-09-25 19:39:01 -0300

    :sparkles: feat: added unocss config

---
+++ uno.config.ts
@@ -0,0 +1,26 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetIcons({
+      prefix: "",
+      autoInstall: true,
+    }),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        "space-mono": "Space Mono:400",
+        "space-grotesk": "Space Grotesk:400,500",
+      },
+    }),
+  ],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
+});
