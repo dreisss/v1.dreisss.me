29bce82c4cd1be169119a114b8dcb501aee8a340
Davi Reis <dreisss026@gmail.com>
2023-09-26 13:05:31 -0300

    :sparkles: feat: added header image

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config'
 import unocss from 'unocss/astro'
 
 export default defineConfig({
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/squoosh'
+    }
+  },
   integrations: [
     unocss({
       injectReset: true
