86c7d0d81ebc2e8638dc6da395bbad7aa22e5ba7
Davi Reis <dreisss.dev@gmail.com>
2024-01-07 14:49:07 -0300

    fix: fixed site domain and updated branch

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, passthroughImageService } from 'astro/config'
 import unocss from 'unocss/astro'
 
 export default defineConfig({
-  site: 'https://v1.dreisss.me',
+  site: 'https://dreisss.me',
   image: {
     service: passthroughImageService()
   },
